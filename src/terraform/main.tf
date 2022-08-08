acf211cfcdd8793e4b768d2f150061ac70a43885
Amarender <dodleamarender.reddy@gmail.com>
2022-08-08 17:18:56 +1000

    Commit

---
+++ src/terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name = "First-group"
+      storage_account_name = "DataStore"
+      container_name = "Statefile_tf"
+      key = "dev.terraform.statefile"
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+  #subscription_id  = var.subscription_id
+  #client_id        = var.client_id
+  #client_secrets   = var.client_secret
+  #tennent_id       = var.tennent_id
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources01"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "VNET01" {
+  name                = "VNET01"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+ 
+}
