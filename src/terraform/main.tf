5ec54abbce848480bd74a35778a28f39ccb3298a
Amarender <dodleamarender.reddy@gmail.com>
2022-08-08 18:00:19 +1000

    Commit code

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -16,16 +16,16 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources01"
+resource "azurerm_resource_group" "TF-resource" {
+  name     = "Terraform resources"
   location = "West Europe"
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "VNET01" {
-  name                = "VNET01"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_virtual_network" "VNET02" {
+  name                = "VNET_02"
+  resource_group_name = azurerm_resource_group.TF-resource.name
+  location            = azurerm_resource_group.TF-resource.location
+  address_space       = ["10.1.0.0/16"]
  
 }
