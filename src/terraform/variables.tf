e2dc9aa3b323f42fbe60c622af2841842578a232
Amarender <dodleamarender.reddy@gmail.com>
2022-08-09 14:50:52 +1000

    commit

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "rgname" {
   type = string
+  #default = "WSTYN-RESOURCES"
   description = "Resource group"
   }
 
 variable "rglocation" {
   type = string
+  #default = "East US"
   description = "Resource group location"
   }
