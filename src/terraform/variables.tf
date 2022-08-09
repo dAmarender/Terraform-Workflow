0c152931fcb248e43a75bcf6bbe5d6625962d445
Amarender <dodleamarender.reddy@gmail.com>
2022-08-09 14:30:54 +1000

    commit

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -1,8 +1,22 @@
 variable "rgname" {
   type = string
-  default = "WSTYN-RESOURCES"
-}
+  description = "Resource group"
+  }
+
 variable "rglocation" {
   type = string
-  default = "East US"
-}
+  description = "Resource group location"
+  }
+
+variable "wstn-vnets" {
+     type = string
+     description = "Creating vnets"
+  }
+
+variable "addressspace" {
+    type = string
+    description = "assigining address spaces"
+  }
+variable "addressprefix" {
+    type = string
+  }
