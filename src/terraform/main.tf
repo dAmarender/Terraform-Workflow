f10cffc105b89e7bda85dc6f9f4a99dcf5fa1392
Amarender <dodleamarender.reddy@gmail.com>
2022-08-09 20:31:00 +1000

    c

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -23,7 +23,7 @@ terraform {
  }
  resource "azurerm_virtual_network" "ws-vnets" {
    name                = "${var.wsvnets}"
-   address_space       = "[10.0.0.0/16]"
+   address_space       = ["10.0.0.0/16"]
    location            = azurerm_resource_group.Wellstyn01.location
    resource_group_name = azurerm_resource_group.Wellstyn01.name
  }
