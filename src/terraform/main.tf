d1b6606c3ff94a70e3905afd9b195c7b08e8a16c
Amarender <dodleamarender.reddy@gmail.com>
2022-08-09 14:31:06 +1000

    commmit

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -18,3 +18,133 @@ terraform {
    name     = "${var.rgname}"
    location = "${var.rglocation}"
  }
+
+ resource "azurerm_virtual_network" "vnets" {
+   name                = "wstynvn"
+   address_space       = "${var.addressspace}"
+   location            = azurerm_resource_group.Wellstyn.location
+   resource_group_name = azurerm_resource_group.Wellstyn.name
+ }
+
+ resource "azurerm_subnet" "subnets" {
+   name                 = "wstynsub"
+   resource_group_name  = azurerm_resource_group.Wellstyn.name
+   virtual_network_name = azurerm_virtual_network.vnets.name
+   address_prefixes     = "${var.addressprefix}"
+ }
+
+ resource "azurerm_public_ip" "wstn-pip" {
+   name                         = "publicIPForLB"
+   location                     = azurerm_resource_group.Wellstyn.location
+   resource_group_name          = azurerm_resource_group.Wellstyn.name
+   allocation_method            = "Static"
+ }
+
+ resource "azurerm_lb" "test" {
+   name                = "loadBalancer"
+   location            = azurerm_resource_group.Wellstyn.location
+   resource_group_name = azurerm_resource_group.Wellstyn.name
+
+   frontend_ip_configuration {
+     name                 = "publicIPAddress"
+     public_ip_address_id = azurerm_public_ip.wstn-pip.id
+   }
+ }
+
+ resource "azurerm_lb_backend_address_pool" "test" {
+   loadbalancer_id     = azurerm_lb.test.id
+   name                = "BackEndAddressPool"
+ }
+
+ resource "azurerm_network_interface" "test" {
+   count               = 2
+   name                = "acctni${count.index}"
+   location            = azurerm_resource_group.test.location
+   resource_group_name = azurerm_resource_group.test.name
+
+   ip_configuration {
+     name                          = "testConfiguration"
+     subnet_id                     = azurerm_subnet.test.id
+     private_ip_address_allocation = "dynamic"
+   }
+ }
+
+ resource "azurerm_managed_disk" "test" {
+   count                = 2
+   name                 = "datadisk_existing_${count.index}"
+   location             = azurerm_resource_group.test.location
+   resource_group_name  = azurerm_resource_group.test.name
+   storage_account_type = "Standard_LRS"
+   create_option        = "Empty"
+   disk_size_gb         = "1023"
+ }
+
+ resource "azurerm_availability_set" "avset" {
+   name                         = "avset"
+   location                     = azurerm_resource_group.Wellstyn.location
+   resource_group_name          = azurerm_resource_group.Wellstyn.name
+   platform_fault_domain_count  = 2
+   platform_update_domain_count = 2
+   managed                      = true
+ }
+
+ resource "azurerm_virtual_machine" "test" {
+   count                 = 2
+   name                  = "acctvm${count.index}"
+   location              = azurerm_resource_group.Wellstyn.location
+   availability_set_id   = azurerm_availability_set.avset.id
+   resource_group_name   = azurerm_resource_group.Wellstyn.name
+   network_interface_ids = [element(azurerm_network_interface.test.*.id, count.index)]
+   vm_size               = "Standard_DS1_v2"
+
+   # Uncomment this line to delete the OS disk automatically when deleting the VM
+   # delete_os_disk_on_termination = true
+
+   # Uncomment this line to delete the data disks automatically when deleting the VM
+   # delete_data_disks_on_termination = true
+
+   storage_image_reference {
+     publisher = "Canonical"
+     offer     = "UbuntuServer"
+     sku       = "16.04-LTS"
+     version   = "latest"
+   }
+
+   storage_os_disk {
+     name              = "myosdisk${count.index}"
+     caching           = "ReadWrite"
+     create_option     = "FromImage"
+     managed_disk_type = "Standard_LRS"
+   }
+
+   # Optional data disks
+   storage_data_disk {
+     name              = "datadisk_new_${count.index}"
+     managed_disk_type = "Standard_LRS"
+     create_option     = "Empty"
+     lun               = 0
+     disk_size_gb      = "1023"
+   }
+
+   storage_data_disk {
+     name            = element(azurerm_managed_disk.test.*.name, count.index)
+     managed_disk_id = element(azurerm_managed_disk.test.*.id, count.index)
+     create_option   = "Attach"
+     lun             = 1
+     disk_size_gb    = element(azurerm_managed_disk.test.*.disk_size_gb, count.index)
+   }
+
+   os_profile {
+     computer_name  = "hostname"
+     admin_username = "testadmin"
+     admin_password = "Password1234!"
+   }
+
+   os_profile_linux_config {
+     disable_password_authentication = false
+   }
+
+   tags = {
+     environment = "staging"
+   }
+ }
